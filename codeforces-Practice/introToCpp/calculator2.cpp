2571d287b21c4d8ebe8895603a3c0f24a367a1b8
Bishwa7 <bishwa244@gmail.com>
2026-09-23 15:20:25 +0530

    cf-prac/introToCpp added

---
+++ codeforces-Practice/introToCpp/calculator2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    long long n, m;
+    cin>>n;
+    cin>>m;
+    
+    cout<<n<<" + "<<m<<" = "<<n + m<<endl<<endl;
+    cout<<n<<" - "<<m<<" = "<<n - m<<endl<<endl;
+    cout<<n<<" * "<<m<<" = "<<n * m<<endl<<endl;
+    cout<<n<<" / "<<m<<" = "<<n / m<<endl<<endl;
+    cout<<n<<" % "<<m<<" = "<<n % m<<endl;
+    
+    return 0;
+}
---
+++ codeforces-Practice/introToCpp/printFirstFiveAlphabets.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    char alphabet = 'A';
+    
+    
+    char word;
+    
+    for(int i=0; i<5; i++){
+        
+        word = (char)(alphabet+i);
+        
+        cout<< word <<endl;
+    }
+    
+    return 0;
+}
